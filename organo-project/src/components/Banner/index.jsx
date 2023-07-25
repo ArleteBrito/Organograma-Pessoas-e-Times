2e863089361fc99c53732cd7b8f840dcea7a63e3
Arlete Brito <arletebrittto@gmail.com>
2023-07-25 13:20:42 -0300

    Inciando projeto Organograma Pessoas e Times

---
+++ organo-project/src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import '../../styles/banner.css';
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organograma" />
+        </header>
+    )
+}
+
+export default Banner; 
