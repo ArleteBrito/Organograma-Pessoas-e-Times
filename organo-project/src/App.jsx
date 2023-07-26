d78c8c48fd5ba48328f5431d6d46c72b78b45fce
Arlete Brito <arletebrittto@gmail.com>
2023-07-26 11:10:25 -0300

    Projeto Organograma Finalizado

--- organo-project/src/App.jsx
+++ organo-project/src/App.jsx
@@ -7,39 +7,39 @@ function App() {
 
   const times = [
     {
-      nome:'Programação',
-      corPrimaria:'#57C278',
-      corSecundaria:'#D9F7E9',
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
     },
     {
-      nome:'Front-End',
-      corPrimaria:'#82CFFA',
-      corSecundaria:'#E8F8FF',
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
     },
     {
-      nome:'Data-Science',
-      corPrimaria:'#A6D157',
-      corSecundaria:'#F0F8E2',
+      nome: 'Data-Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
     },
     {
-      nome:'Devops',
-      corPrimaria:'#E06B69',
-      corSecundaria:'#FDE7E8',
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
     },
     {
-      nome:'UX e Desing',
-      corPrimaria:'#D86EBF',
-      corSecundaria:'#FAE5F5',
+      nome: 'UX e Desing',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
     },
     {
-      nome:'Mobile',
-      corPrimaria:'#FEBA05',
-      corSecundaria:'#FFF5D9',
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
     },
     {
-      nome:'Inovação e Gestão',
-      corPrimaria:'#FF8A29',
-      corSecundaria:'#FFEEDF',
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
     }
   ];
 
@@ -48,13 +48,22 @@ function App() {
   const aoNovoColaboradorAdicionado = (colaborador) => {
     setColaboradores([...colaboradores, colaborador]);
   }
- 
+
   return (
     <>
       <div>
-        <Banner/>
-        <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-        {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+        <Banner />
+        <Formulario times={times.map(time => time.nome)} 
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} 
+        />
+        {times.map(time => <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />
+        )}
       </div>
     </>
   )
--- organo-project/src/components/Formulario/index.jsx
+++ organo-project/src/components/Formulario/index.jsx
@@ -6,21 +6,11 @@ import '../../styles/formulario.css';
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data-Science',
-        'Devops',
-        'UX e Desing',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [nome, setNome] = useState('');
     const [cargo, setCargo] = useState('');
     const [imagem, setImagem] = useState('');
     const [time, setTime] = useState('');
-    
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
         props.aoColaboradorCadastrado({
@@ -29,39 +19,43 @@ const Formulario = (props) => {
             imagem,
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Nome" 
-                placeholder="Digite o seu nome" 
-                valor={nome}
-                aoAlterado={valor => setNome(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite o seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
                 />
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Cargo" 
-                placeholder="Digite o seu cargo" 
-                valor={cargo}
-                aoAlterado={valor => setCargo(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite o seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
                 />
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Imagem" 
-                placeholder="Digite o endereço da imagem" 
-                valor={imagem}
-                aoAlterado={valor => setImagem(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
                 />
-                <ListaSuspensa 
-                obrigatorio={true} 
-                label="Time" 
-                itens={times}
-                valor={time}
-                aoAlterado={valor => setTime(valor)}
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
                 />
                 <Botao>
                     Criar Card
